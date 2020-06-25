357a32500d913d27aa55f831d85066f059c65259
Jack Nelson <jnelson.java@gmail.com>
2020-06-25 12:22:37 -0700

    refactored JS into functions

--- app.js
+++ app.js
@@ -2,43 +2,74 @@
 
 // This section creates a message in the header to let the user know if the brewery is currently open
 
-var today = new Date();
-var hourNow = today.getHours();
-var openStatus;
+function openHour() {
+    var today = new Date();
+    var hourNow = today.getHours();
+    return hourNow;
+}
+
+var hourNow = openHour();
 
 // line below for testing a date failure
 // hourNow = 'tom'
 
-if (hourNow >= 20) {
-    openStatus = 'The brewery is closed for the night.';
-} else if (hourNow >= 12) {
-    openStatus = 'The brewery is currently open for pick up.';
-} else if (hourNow >= 0) {
-    openStatus = "Don't you think it's a bit early? Check again later.";
-} else {
-    openStatus = 'Operation hours available below! Nothing wrong here ... just scroll down.';
+function openMessage(hourNow) {
+    var openStatus
+    if (hourNow >= 20) {
+        openStatus = 'The brewery is closed for the night.';
+    } else if (hourNow >= 12) {
+        openStatus = 'The brewery is currently open for pick up.';
+    } else if (hourNow >= 0) {
+        openStatus = "Don't you think it's a bit early? Check again later.";
+    } else {
+        openStatus = 'Operation hours available below! Nothing wrong here ... just scroll down.';
+    }
+    return openStatus;
 }
 
-// document.write('<h3>' + openStatus + '</h3>');
+var openStatus = openMessage(hourNow);
 
-var elOpenStatus = document.getElementById('openStatus');
-elOpenStatus.textContent = openStatus
+
+function replaceTextByEl(elID, message) {
+    var elToReplace = document.getElementById(elID);
+    elToReplace.textContent = message;
+}
+
+replaceTextByEl('openStatus', openStatus);
+
+// var elOpenStatus = document.getElementById('openStatus');
+// elOpenStatus.textContent = openStatus
 
 
 // This will prompt the user for their name and personalize a message.
 
-var userName;
-userName = prompt("Please enter your name!")
+function getUserName() {
+    var userName = prompt("Please enter your name!");
+    return userName;
+}
+
+var userName = getUserName();
+
+// var userName;
+// userName = prompt("Please enter your name!")
 
 // line below for testing name failure
 // userName = 20
 
-if (typeof userName !== 'string' || userName === '') {
-    userName = 'friend';
-    confirm("I'll just call you friend ...");
-} else {
-    confirm("Welcome, " + userName + "!");
+
+function checkForNameEntered(userName) {
+    if (typeof userName !== 'string' || userName === '') {
+        userName = 'friend';
+        confirm("I'll just call you friend ...");
+    } else {
+        confirm("Welcome, " + userName + "!");
+    }
+    return userName
 }
 
-var elName = document.getElementById('name');
-elName.textContent = userName;
+userName = checkForNameEntered(userName);
+
+replaceTextByEl('name', userName);
+
+// var elName = document.getElementById('name');
+// elName.textContent = userName;
